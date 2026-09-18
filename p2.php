f3de423ee170a773c155471fa19d2732aa8d6a24
Luke Tidball <lrtidball@ku.edu>
2026-09-18 15:15:53 -0500

    Added base stuff. I am so scared about deploying this to the server.

---
+++ p2.php
@@ -0,0 +1,15 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Practice 2</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<main>
+  <nav class="pages">
+    <ul>
+      <li><a href="index.html">Practice1</a></li>
+      <li><a href="p2.php">Practice2</a></li>
+      <li><a href="p3.php">Practice3</a></li>
+      <li><a href="p4.php">Practice4</a></li>
+    </ul>
+  </nav>
